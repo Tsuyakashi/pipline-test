3ea25e1eef413a9dac8e0c1caedc1eaed6b13179
Tsu <kashakdaniil@gmail.com>
2025-11-05 00:04:23 +0300

    setup & deploy update

--- setup-aws/setup.sh
+++ setup-aws/setup.sh
@@ -1,7 +1,8 @@
-sudo yum update && sudo upgrade -y
-sudo yum install docker git -y
-sudo systemctl start docker
-
-sudo adduser frontend
-sudo usermod -a -G docker frontend
-sudo su - frontend
+#!/bin/bash
+udo yum update && sudo upgrade -y
+echo "Install docker & git" && sudo yum install docker git -y
+echo "Start docker daemon" && sudo systemctl start docker
+echo "Pull image"&& sudo docker pull tsuyakashi/pipline-test
+echo "Start frontend container" && \
+sudo docker run -d -p 80:8080 --name frontend tsuyakashi/pipline-test
+echo "Finish deploying!"
